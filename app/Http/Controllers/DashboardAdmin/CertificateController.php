1d6e460372d403020bed6f88f997b24d9a7f5c5c
cristianDH790 <luisito_619_@hotmail.com>
2024-05-03 12:32:51 -0500

    add certificate adn modules1

---
+++ app/Http/Controllers/DashboardAdmin/CertificateController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\DashboardAdmin;
+
+use App\Models\certificate;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CertificateController extends Controller
+{
+    public function index()
+    {
+        return view('dashboardAdmin.certificate.index');
+    }
+    public function create()
+    {
+        return view('dashboardAdmin.certificate.create');
+    }
+    public function edit($id)
+    {
+        $certificate = certificate::where('id', $id)->firstOrFail();
+        return view('dashboardAdmin.certificate.edit', compact('certificate'));
+    }
+}
