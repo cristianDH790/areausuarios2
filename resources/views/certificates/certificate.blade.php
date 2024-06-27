10166199f05c6a94ea0b7fa61f366dc5602484b1
Cristian DH <80552859+cristianDH790@users.noreply.github.com>
2024-06-26 19:53:21 -0500

    primer commit (#13)

--- resources/views/certificates/certificate.blade.php
+++ resources/views/certificates/certificate.blade.php
@@ -5,12 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 </head>
 
 <body>
-    hola
+
+    <h1>hola</h1>
 </body>
 
 </html>
